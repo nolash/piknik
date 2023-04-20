76dc2d2cd2c5113ae336aa24e36e6e15eead3f5b
lash <dev@holbrook.no>
2023-04-20 08:16:12 +0100

    Run cli tests in temp dir store

--- test_cli.sh
+++ test_cli.sh
@@ -6,15 +6,15 @@ set -x
 default_pythonpath=$PYTHONPATH:.
 export PYTHONPATH=${default_pythonpath:-.}
 d=`mktemp -d`
-i_foo=`python piknik/runnable/cmd.py add "foo"`
-i_bar=`python piknik/runnable/cmd.py add "bar"`
-python piknik/runnable/cmd.py mod --accept -i $i_foo
-python piknik/runnable/cmd.py mod --finish -i $i_bar
-python piknik/runnable/cmd.py comment -x bazbazbaz -i $i_foo
-python piknik/runnable/cmd.py show
-python piknik/runnable/cmd.py show -r html
-python piknik/runnable/cmd.py show -i $i_foo
-python piknik/runnable/cmd.py show -r html -i $i_bar
+i_foo=`python piknik/runnable/cmd.py -d $d  add "foo"`
+i_bar=`python piknik/runnable/cmd.py -d $d  add "bar"`
+python piknik/runnable/cmd.py mod -d $d --accept -i $i_foo
+python piknik/runnable/cmd.py mod -d $d  --finish -i $i_bar
+python piknik/runnable/cmd.py comment -d $d  -x bazbazbaz -i $i_foo
+python piknik/runnable/cmd.py show -d $d 
+python piknik/runnable/cmd.py show -d $d  -r html
+python piknik/runnable/cmd.py show -d $d  -i $i_foo
+python piknik/runnable/cmd.py show -d $d  -r html -i $i_bar
 set +x
 set +e
 set +a
