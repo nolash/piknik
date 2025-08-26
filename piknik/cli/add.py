0784cb4d3a9e62927bc925db923b54fc54c8c9cb
lash <dev@holbrook.no>
2025-08-26 13:51:57 +0100

    Set backlog state by default unless explicitly proposed

--- piknik/cli/add.py
+++ piknik/cli/add.py
@@ -9,11 +9,13 @@ ctx = None
 
 def subparser(argp):
     argp.add_argument('title', type=str, nargs='*', help='issue title')
+    argp.add_argument('--proposed', type=bool, help='start issue in proposed state')
     return argp
 
 
 def assembler(o, arg):
     o.title = arg.title
+    o.proposed = arg.proposed
 
 
 def main():
@@ -29,5 +31,10 @@ def main():
         o = Issue(title, issue_id=issue_id, alias=issue_id[:5])
     else:
         o = Issue(title, alias=ctx.alias)
+
     v = ctx.basket.add(o)
+
+    if not ctx.proposed:
+        ctx.basket.state_backlog(issue_id)
+
     sys.stdout.write(v + '\n')
